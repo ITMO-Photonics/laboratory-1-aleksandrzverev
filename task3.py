6b11df7d009d80bca6c4759db98a3de90f9b6114
aleksandrzverev <37011582+aleksandrzverev@users.noreply.github.com>
2018-04-22 18:01:56 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,11 @@
-
+import numpy as np
+import scipy.linalg as lin
+n = 100
+A = np.zeros((n,n))
+i,j=np.indices(A.shape)
+A[i==j]=1
+A[i==j+1]=1
+A[i==j+2]=1
+B = np.arange((n))
+Matrix = lin.solve(A,B)
+Matrix
